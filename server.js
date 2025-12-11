a8a89bd1091fe8be8a6ca29edae4e520c63b9fd5
leorestored <oubeisi.a@gmail.com>
2025-12-11 19:57:25 +1100

    updated posting

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { postLeoThought, initTwitterClient } from './x-automation.js';
-import { postLeoThought, initTwitterClient } from './x-automation.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
